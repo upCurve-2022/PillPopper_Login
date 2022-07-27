32990785a9e692aaac7907ba9e3788b4509279b7
Jaspriya Gujral <jaspriya12@gmail.com>
2022-07-27 22:11:51 +0530

    Add files via upload

---
+++ Popper/src/main/java/com/Pill/Popper/dao/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package com.Pill.Popper.dao.repository;
+
+import com.Pill.Popper.dao.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    Optional<UserEntity> findByUsername(String username);
+
+    Boolean existsByUsername(String username);
+
+    Boolean existsByEmail(String email);
+}
