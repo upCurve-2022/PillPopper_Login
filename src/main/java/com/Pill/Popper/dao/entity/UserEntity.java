29a9ddf9d7dc2bf5460ef41e70ca0af76c230a27
Jaspriya12 <jaspriya12@gmail.com>
2022-07-27 20:51:32 +0530

    Initial commit

---
+++ src/main/java/com/Pill/Popper/dao/entity/UserEntity.java
@@ -0,0 +1,63 @@
+package com.Pill.Popper.dao.entity;
+
+import com.Pill.Popper.dao.validation.ValidPassword;
+import lombok.*;
+
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+@Table(	name = "user_info",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = "username"),
+                @UniqueConstraint(columnNames = "email")
+        })
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(max = 20)
+    private String username;
+
+    @NotBlank
+    @Size(max = 50)
+    @Email
+    private String email;
+
+    @ValidPassword
+    @NonNull
+    @NotBlank(message = "New password is mandatory")
+    @Min(5)
+    private String password;
+
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "USER_ROLE",
+            joinColumns = {
+                    @JoinColumn(name = "USER_ID")
+            },
+            inverseJoinColumns = {
+                    @JoinColumn(name = "ROLE_ID")
+            }
+    )
+    private Set<RoleEntity> roles= new HashSet<>();
+    public UserEntity() {
+    }
+
+    public UserEntity(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+}
---
+++ src/main/java/com/Pill/Popper/dao/service/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package com.Pill.Popper.dao.service;
+
+import com.Pill.Popper.dao.entity.UserEntity;
+import com.Pill.Popper.dao.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserEntity user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+
+        return UserDetailsImpl.build(user);
+    }
+
+}
